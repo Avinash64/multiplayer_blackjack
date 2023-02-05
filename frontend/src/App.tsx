2b6263dae24b234e623a4a3e52f012db00c38169
dev-rb <43100342+dev-rb@users.noreply.github.com>
2023-02-05 18:34:37 -0500

    Init react app. Add unocss, prettier, eslint.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+function App() {
+  return (
+    <div className="w-screen h-screen bg-dark-9">
+      <h1 className="color-white"> Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
